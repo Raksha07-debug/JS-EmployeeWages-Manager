de2bc892d77df4e3b27875b8aebfd80eddfaf930
Raksha <rak070803@gmail.com>
2025-03-05 13:54:25 +0530

    [Raksha] add. Calculating wages for a month assuming 20 working days in a month

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -26,3 +26,11 @@ let empCheckUC3=Math.floor(Math.random()*10)%3;
 let empHrsUC3=getWorkingHours(empCheck);
 let empWageUC3=empHrsUC3*WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWageUC3);
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+    }
+let dailyWage = getWorkingHours(empCheck) * WAGE_PER_HOUR;
+console.log(`Total Monthly Hours: ${totalEmpHrs}, Total Monthly Wage: ${totalEmpHrs * WAGE_PER_HOUR}`);
