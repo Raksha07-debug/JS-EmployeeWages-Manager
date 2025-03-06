7d246d8e01b78a2950943d90757af4b0681bf093
Raksha <rak070803@gmail.com>
2025-03-06 11:08:49 +0530

    [Raksha] add. JS EmployeeWages Ability to check the name starts with capital and has at least 3 characters

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -4,7 +4,7 @@ class EmployeePayrollData {
     gender;
     startDate;
 
-    constructor(params) {
+    constructor(...params) {
         this.id = params[0];
         this.name = params[1];
         this.salary = params[2];
@@ -13,23 +13,32 @@ class EmployeePayrollData {
     }
 
     get name() { return this._name; }
-    set name(name) { this._name = name; }
+    set name(name) {
+        let nameRegex = RegExp(/^[A-Z]{1}[a-z]{3,}$/); 
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect!';
+    }
 
     toString() {
-        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary;
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        const empDate = this.startDate ? this.startDate.toLocaleDateString("en-US", options) : "undefined";
         return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + ", gender=" + this.gender + ", startDate=" + empDate;
-    
     }
 }
-
 
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 
-employeePayrollData.name = "John";
+employeePayrollData.name = "John"; // Valid name
 console.log(employeePayrollData.toString());
 
-let newEmployeePayrollData = new EmployeePayrollData([2, "Terrisa", 30000, "F", new Date()]);
+try {
+    employeePayrollData.name = "Rohn"; 
+    console.log(employeePayrollData.toString());
+} catch (e) {
+    console.error(e);
+}
+
+let newEmployeePayrollData = new EmployeePayrollData(2, "Terrisa", 30000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
